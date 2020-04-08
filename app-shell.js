417d6f3ee57d2ae478aeef906ff65149d618719d
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-04-08 14:18:34 +0200

    Fix

---
+++ app-shell.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class AppShell extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null);
+  }
+
+}
+
+export default AppShell;
